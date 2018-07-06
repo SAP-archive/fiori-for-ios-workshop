8a7e697fb635a59fa0c74ca414c59f34ff77b6eb
i826181 <stan.stadelman@sap.com>
2018-07-06 13:46:17 -0700

    parameterized destination complete

--- TravelExpenses/AppDelegate.swift
+++ TravelExpenses/AppDelegate.swift
@@ -14,6 +14,7 @@ import SAPOfflineOData
 import UserNotifications
 
 let APPLICATIONID: String = "com.sap.expense"
+let DESTINATION = "com.sap.expense"
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate, OnboardingManagerDelegate, UNUserNotificationCenterDelegate {
